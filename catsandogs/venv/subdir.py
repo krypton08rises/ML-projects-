c0929e2fe1c6434a116c38622df9734365a14ce7
Kushagra Raina <kushagra.raina@gmail.com>
2019-09-18 22:08:49 +0530

    cats and dogs dataset from kaggle

---
+++ catsandogs/venv/subdir.py
@@ -0,0 +1,9 @@
+# create directories
+dataset_home = 'dataset_dogs_vs_cats/'
+subdirs = ['train/', 'test/']
+for subdir in subdirs:
+	# create label subdirectories
+	labeldirs = ['dogs/', 'cats/']
+	for labldir in labeldirs:
+		newdir = dataset_home + subdir + labldir
+		makedirs(newdir, exist_ok=True)
